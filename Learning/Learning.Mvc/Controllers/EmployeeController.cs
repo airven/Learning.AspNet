1dcab1b0ea990e2ba1a35767351e5ea5a6449560
airven <1017091284@qq.com>
2021-07-07 17:30:28 +0800

    add code

---
+++ Learning/Learning.Mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Learning.Mvc.Models;
+namespace Learning.Mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        public EmployeeController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetAllEmployees()
+        {
+            try
+            {
+                var item = db.Employees.Select(x=>new { x.EmployeeID,x.FirstName,x.LastName,x.Address,x.City,x.Region}).ToList();
+                return Json(item, JsonRequestBehavior.AllowGet);
+            }
+            catch(Exception ex)
+            {
+                return Json(null);
+            }
+           
+        }
+        public ActionResult LoadEditEmployeesPopup(int EmployeesId)
+        {
+            try
+            {
+                var model = db.Employees.Where(a => a.EmployeeID == EmployeesId).FirstOrDefault();
+                return PartialView("_UpdateEmployee", model);
+            }
+            catch (Exception ex)
+            {
+                return PartialView("_UpdateEmployee");
+            }
+        }
+        public ActionResult LoadaddEmployeesPopup()
+        {
+            try
+            {
+                return PartialView("_AddEmployee");
+            }
+            catch (Exception ex)
+            {
+                return PartialView("_AddEmployee");
+            }
+        }
+
+        public JsonResult AddEmployees(Employee employee)
+        {
+            string status = "success";
+            try
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                status = ex.Message;
+            }
+            return Json(status, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult UpdateEmployees(Employee employee)
+        {
+            string status = "success";
+            try
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                status = ex.Message;
+
+            }
+            return Json(employee, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult DeleteEmployees(int EmployeesId)
+        {
+            string status = "success";
+            try
+            {
+                var pateint = db.Employees.Find(EmployeesId);
+                db.Employees.Remove(pateint);
+                db.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                status = ex.Message;
+
+            }
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Learning/Learning.Mvc/Controllers/HomeController.cs
+++ Learning/Learning.Mvc/Controllers/HomeController.cs
@@ -1,18 +1,108 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Learning.Mvc.Models;
+
 
 namespace Learning.Mvc.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        private NorthwindEntities db = new NorthwindEntities();
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile()
+        {
+            try
+            {
+                string FullName = Request.Form["FullName"];
+                string Email = Request.Form["Email"];
+                string Password = Request.Form["Password"];
+                if (!IsEmailExist(Email))
+                {
+                    if (Request.Files.Count > 0)            // Checking if  Request object  has file
+                    {
+                        try
+                        {
+                            using (db)
+                            {
+                                HttpPostedFileBase file = Request.Files[0];
+                                string fname = file.FileName;
+                                // Get the complete folder path and store the file inside it.  
+                                fname = Path.Combine(Server.MapPath("~/Images/"), fname);
+                                file.SaveAs(fname);
+                                Employee userobj = new Employee
+                                {
+                                    //Email_Id = Email,
+                                    //FullName = FullName,
+                                    //PhotoPath = "/Images/" + file.FileName
+                                };
+                                db.Employees.Add(userobj);
+                                if (db.SaveChanges() > 0)
+                                {
+                                    //Set MVC and Login Authentication
+                                    return Json("User registered Successfully!");
+                                }
+                                else
+                                {
+                                    return Json("Something went wrong please try again later!");
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            return Json(ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        return Json("No files selected.");
+                    }
+                }
+                else
+                {
+                    return Json("email already exist please try with diffrent one!");
+                }
+
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+                return Json(null);
+            }
+            return View();
+        }
+
+
+        private bool IsEmailExist(string email)
+        {
+            bool IsEmailExist = false;
+            using (db)
+            {
+                int count = db.Employees.Where(a => a.HomePhone == email).Count();
+                if (count > 0)
+                {
+                    IsEmailExist = true;
+                }
+            }
+            return IsEmailExist;
         }
     }
 }
